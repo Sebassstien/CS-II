5f58056d5606666d37ac57e26614e53ce840a448
Sebassstien <sebastien.lafontainesanchez@gmail.com>
2025-02-01 16:04:16 -0800

    First Prototype without graphics

--- test_maze.py
+++ test_maze.py
@@ -1,5 +1,34 @@
 with open("maze.txt", "r") as file:
     file_contents = file.read()
 
-for line in file_contents.splitlines():
-    print(line)
+for lineNum in range(len(file_contents.splitlines())):
+    line = file_contents.splitlines()[lineNum]
+    for c in range(len(line)):
+        right = False
+        left = False
+        up = False
+        down = False
+        if '#' != line[c]:
+            print(' ', end='')
+            continue
+        if c-1 >= 0:
+            if '#' == line[c-1]:
+                left = True
+        if c+1 < len(line):
+            if '#' == line[c+1]:
+                right = True                
+        if lineNum - 1 >= 0:
+            if '#' == file_contents.splitlines()[lineNum-1][c]:
+                up = True
+        if lineNum + 1 < len(file_contents.splitlines()):
+            if '#' == file_contents.splitlines()[lineNum+1][c]:
+                down = True
+        if up == True or True == down and left == True or True == right:
+            print('+', end='')
+        elif up == True or True == down:
+            print('|', end='')
+        elif left or right:
+            print('-', end='')
+        elif up == False and False == down and left == False and False == right:
+            print('*', end='')
+    print('')
