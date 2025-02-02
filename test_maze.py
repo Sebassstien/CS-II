232eb33370f692255fcf2f854328f36fcdc45851
Sebassstien <sebastien.lafontainesanchez@gmail.com>
2025-02-01 20:16:43 -0800

    Added more elif statements and cleaned the code a little bit

--- test_maze.py
+++ test_maze.py
@@ -1,6 +1,6 @@
 with open("maze.txt", "r") as file:
     file_contents = file.read()
-
+    #Check if there is a '#' to set the value as True
 for lineNum in range(len(file_contents.splitlines())):
     line = file_contents.splitlines()[lineNum]
     for c in range(len(line)):
@@ -23,12 +23,31 @@ for lineNum in range(len(file_contents.splitlines())):
         if lineNum + 1 < len(file_contents.splitlines()):
             if '#' == file_contents.splitlines()[lineNum+1][c]:
                 down = True
-        if up == True or True == down and left == True or True == right:
+
+        # Draw the walls and connections
+        if up and down and left and right:
             print('+', end='')
-        elif up == True or True == down:
+        elif up and left and right:
+            print('⫠', end='')
+        elif up and left and down:
+            print('⊣', end='')
+        elif up and right and down:
+            print('⊢', end='')
+        elif up and left:
+            print('⨼', end='')
+        elif up and right:
+            print('⨽', end='')
+        elif down and left and right:
+            print('⫟', end='')
+        elif down and left:
+            print('ɿ', end='')
+        elif down and right:
+            print('ɾ', end='')
+        elif up or down:
             print('|', end='')
         elif left or right:
-            print('-', end='')
-        elif up == False and False == down and left == False and False == right:
+            print('−', end='')
+        elif not up and not down and not left and not right:
             print('*', end='')
+
     print('')
