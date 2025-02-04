1916dae0686c1635a0866dbc025da4fbe8098aec
Sebassstien <sebastien.lafontainesanchez@gmail.com>
2025-02-04 14:24:09 -0800

    Added Completed Maze Program (Revised by Alain)

--- maze.py
+++ maze.py
@@ -1,9 +1,61 @@
+##  Collaborators:
+##  Sebastien LaFontaine
+##  Spencer Ollmann
+
 import graphics as g
-size = 5
-win = g.GraphWin("Maze", 400, 400)
-win.setCoords(-0.5, -0.5, size + 0.5, size + 0.5)
-l = g.Line(g.Point(1.0, 5.0), g.Point(1.0, 4.0))
-l.setWidth(3)
-l.draw(win)
+
+filename = "maze.txt"
+
+def draw_line(p1, p2, win):
+    line = g.Line(p1, p2)
+    line.setWidth(3)       
+    line.setOutline("black") 
+    line.draw(win)
+    return win
+
+# Open the maze file and read its contents
+with open(filename, "r") as file:
+    file_contents = file.readlines() 
+
+    maze_height = len(file_contents) 
+    maze_width = max(len(line) for line in file_contents)
+
+    win = g.GraphWin("Maze", 400, 400)
+    win.setCoords(-1.5, maze_height - 0.5, maze_width - 0.5, -1.5)
+
+# Loop through each row and column in the maze to find walls and draw horizontal lines
+for y in range(maze_height):
+    line = file_contents[y]
+    start_point = None 
+    for x in range(len(line)): 
+        if line[x] == '#':  
+            if start_point is None:
+                start_point = g.Point(x, y)  
+            end_point = g.Point(x, y)
+        else:
+            if start_point is not None:
+                draw_line(start_point, end_point, win)  
+                start_point = None
+
+    if start_point is not None:
+        draw_line(start_point, end_point, win)
+
+# Loop through each column in the maze to find vertical walls and draw vertical lines
+for x in range(maze_width):
+    start_point = None 
+    for y in range(maze_height):
+        line = file_contents[y][x] if x < len(file_contents[y]) else ' '
+        if line == '#':
+            if start_point is None:
+                start_point = g.Point(x, y)  
+            end_point = g.Point(x, y) 
+        else:
+            if start_point is not None:
+                draw_line(start_point, end_point, win)  
+                start_point = None  
+
+    if start_point is not None:
+        draw_line(start_point, end_point, win)
+
 win.getMouse()
 win.close()
