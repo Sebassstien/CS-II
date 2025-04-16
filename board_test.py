2db4a50bfc24a126e9004dd7ddab5ffa6aa01b53
Spencer Ollmann <spencer.ollmann@me.com>
2025-04-16 10:46:44 -0700

    added test_board_intit()

--- board_test.py
+++ board_test.py
@@ -2,6 +2,8 @@ from board import Piece
 from board import Position
 from board import Board
 from board import Game
+BOARD_HEIGHT = 8
+BOARD_WIDTH = 8
 
 def test_position():
     p1 = Position(1,1)
@@ -12,6 +14,24 @@ def test_position():
     p1 = Position()
     assert p1.x == 0
     assert p1.y == 0
-    
 
-def 
+
+def test_piece():
+    p = Piece()
+    assert p.color == None
+    assert p.position == None
+
+def test_board_init():
+    b = Board(BOARD_HEIGHT,BOARD_WIDTH)
+    assert len(b.board) == BOARD_HEIGHT
+    for i in b.board:
+        assert len(i) == BOARD_WIDTH
+    
+def test_board_initialize_pieces():
+    b = Board(BOARD_HEIGHT,BOARD_WIDTH)
+    b.initialize_pieces()
+    for i in range(1, len(b.board[0])-1):
+        p = b.board[0][i]
+        assert isinstance(p, Piece)
+        if isinstance(p, Piece):
+            assert p.color == "white"
