2677d6f23362f49feeeb08a8d7e051a36dae4f79
Spencer Ollmann <spencer.ollmann@me.com>
2025-04-16 16:08:13 -0700

    more tests

--- board_test.py
+++ board_test.py
@@ -34,7 +34,23 @@ def test_initialize_pieces():
         p = b.board[0][i]
         assert isinstance(p, Piece)
         if isinstance(p, Piece):
+            assert p.color == "black"
+    for i in range(1, len(b.board[0])-1):
+        p = b.board[7][i]
+        assert isinstance(p, Piece)
+        if isinstance(p, Piece):
+            assert p.color == "black"
+    for i in range(1, len(b.board)-1):
+        p = b.board[i][0]
+        assert isinstance(p, Piece)
+        if isinstance(p, Piece):
             assert p.color == "white"
+    for i in range(1, len(b.board)-1):
+        p = b.board[i][7]
+        assert isinstance(p, Piece)
+        if isinstance(p, Piece):
+            assert p.color == "white"
+
 def test_get_piece():
     b = Board(BOARD_HEIGHT,BOARD_WIDTH)
     b.board[0][0] = Piece('white',Position(0,0))
@@ -54,3 +70,33 @@ def test_num_pieces_in_line():
     assert b.num_pieces_in_line(Position(1,1), Position(0,0)) == 3
     assert b.num_pieces_in_line(Position(1,1), Position(0,1)) == 2
     assert b.num_pieces_in_line(Position(3,3), Position(0,3)) == 1
+
+def test_move_pieces():
+    b = Board(BOARD_HEIGHT,BOARD_WIDTH)
+    b.board[0][0] = Piece('white',Position(0,0))
+    b.board[1][1] = Piece('black',Position(1,1))
+    b.board[3][3] = Piece('black', Position(3,3))
+    b.board[0][1] = Piece('white', Position(0,1))
+    b.move_pieces(Position(1,0), Position(0,0))
+    assert b.move_pieces(Position(-7,-7),Position(0,0)) == None
+    assert b.move_pieces(Position(7,7),Position(0,0)) == None
+    assert b.get_piece(Position(0,0)).color == "white"
+
+def test_available_moves():
+    b = Board(BOARD_HEIGHT,BOARD_WIDTH)
+    b.board[0][0] = Piece('white',Position(0,0))
+    b.board[1][1] = Piece('black',Position(1,1))
+    b.board[3][3] = Piece('black', Position(3,3))
+    b.board[0][1] = Piece('white', Position(0,1))
+
+def test_are_connected():
+    pass
+
+def test_is_movable():
+    pass
+
+def test_move_piece():
+    pass
+
+def test_check_winner():
+    pass
